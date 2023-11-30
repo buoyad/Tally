cf3edc07b434f409ce49d94de79346611b9baff4
Danny Ayoub <danny.a95@gmail.com>
2023-11-30 10:42:13 -0600

    I don't know if I want this lint rule on, but appease it for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <p>Tally is a score keeper for the <a href="https://www.nytimes.com/crosswords/game/mini" target="_blank">New York Times Mini Crossword</a>.</p>
-      <p><Link href="/tournaments/create">Create a tournament</Link> or <Link href="/tournaments">browse</Link> other tournaments' scores.</p>
+      <p><Link href="/tournaments/create">Create a tournament</Link> or <Link href="/tournaments">browse</Link> other tournaments&apos; scores.</p>
     </main>
   )
 }
