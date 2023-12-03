ce42bfa2dbbff50a5b4bf9992e49d413722a871b
Danny Ayoub <danny.a95@gmail.com>
2023-12-02 18:03:17 -0600

    customize log in email

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -3,10 +3,14 @@ import EmailProvider from "next-auth/providers/email"
 import PostgresAdapter from "@auth/pg-adapter"
 import { Pool } from "pg"
 import { AuthOptions } from "next-auth"
+import { createTransport } from "nodemailer"
+import log from "@/app/lib/log"
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + (process.env.NODE_ENV === "production" ? "?sslmode=require" : "")
 })
+
+const verifyEmailMaxAge = 15 * 60 // 15 minutes
 
 export const authOptions: AuthOptions = {
     adapter: PostgresAdapter(pool),
@@ -20,16 +24,27 @@ export const authOptions: AuthOptions = {
                     pass: process.env.EMAIL_SERVER_PASSWORD
                 }
             },
-            from: process.env.EMAIL_FROM
+            from: process.env.EMAIL_FROM,
+            sendVerificationRequest: async (params) => {
+                const { identifier, url, provider } = params
+                const transport = createTransport(provider.server)
+                const result = await transport.sendMail({
+                    to: identifier,
+                    from: provider.from,
+                    subject: `Sign in to Tally`,
+                    text: `${url}\nThis link will expire in 15 minutes.\n\n`,
+                    html: `<a href="${url}">Sign in to Tally</a><br><br>This link will expire in 15 minutes.<br><br>`
+                })
+                const failed = result.rejected.concat(result.pending).filter(Boolean)
+                if (failed.length) {
+                    throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`)
+                }
+                log.info(`verification email sent to ${identifier}`)
+            },
         })
     ],
     pages: {
         signIn: '/login',
         verifyRequest: '/login/check-email',
     }
-    // callbacks: {
-    //     async redirect(params) {
-    //         return params.baseUrl + '/user'
-    //     },
-    // }
 }
