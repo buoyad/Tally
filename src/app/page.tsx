83f26df762dbef20138b2dea89f51935a754c42e
Danny Ayoub <danny.a95@gmail.com>
2023-12-03 19:04:02 -0600

    eslint busted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
         {!!session && <p>Welcome, {userInfo.name}.</p>}
         <p>Tally is a score keeper for daily crossword puzzles.</p>
         <p><Link href="/tournaments">Browse tournaments</Link> or <Link href="/tournaments/create">create</Link> one of your own.</p>
-        {!!session && <Subheading><Link href="/score">Register today's score</Link></Subheading>}
+        {!!session && <Subheading><Link href="/score">Register today&apos;s score</Link></Subheading>}
       </Box>
     </main>
   )
