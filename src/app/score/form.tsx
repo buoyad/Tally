6e77313b72fe469429ac966e44dddb3588bed20c
Danny Ayoub <danny.a95@gmail.com>
2023-12-03 20:07:24 -0600

    wip score page

--- src/app/score/form.tsx
+++ src/app/score/form.tsx
@@ -3,13 +3,16 @@ import * as React from 'react'
 import { UserInfo } from "../lib/db";
 import { Box } from "../ui/components";
 import { createWorker, PSM } from 'tesseract.js'
+import styles from '@/app/ui/form.module.css'
 
 const loadWorker = createWorker('eng')
+
+const timeRegex = /(\d{1,2}):(\d{2})/g
 
 export default function Form({ userInfo }: { userInfo: UserInfo }) {
     const [file, setFile] = React.useState<File | null>(null)
     const [processing, setProcessing] = React.useState(false)
-    const [parsedTime, setParsedTime] = React.useState('')
+    const [parsedTime, setParsedTime] = React.useState(-1)
 
     React.useEffect(() => {
         async function runOCR() {
@@ -20,20 +23,30 @@ export default function Form({ userInfo }: { userInfo: UserInfo }) {
                 tessedit_pageseg_mode: PSM.SINGLE_COLUMN,
             })
             const { data: { text } } = await worker.recognize(file!)
-            setParsedTime(text)
-            await worker.terminate()
+
+            const matches = [...(text.matchAll(timeRegex))]
+            let lastMatch = matches[matches.length - 1]
+            if (lastMatch) {
+                const [_, minutes, seconds] = lastMatch
+                const parsedMinutes = parseInt(minutes)
+                const parsedSeconds = parseInt(seconds)
+                const totalSeconds = parsedMinutes * 60 + parsedSeconds
+                setParsedTime(totalSeconds)
+            }
+
             setProcessing(false)
         }
-        if (file) {
+        if (file && file.type.startsWith('image/')) {
             runOCR()
         }
     }, [file])
 
     return <form>
         <Box>
-            <input type="file" name="file" accept="image/*" onChange={(e) => e.target.files && setFile(e.target.files[0])} />
+            <p>Select your completion screenshot</p>
+            <input type="file" name="file" accept="image/*" onChange={(e) => e.target.files && setFile(e.target.files[0])} title='Upload' className={styles.fileInput} />
             {processing && <p>Processing...</p>}
-            {parsedTime && <p>{parsedTime}</p>}
+            {parsedTime > 0 && <p>{Math.floor(parsedTime / 60)} minutes and {Math.floor(parsedTime % 60)} seconds</p>}
         </Box>
     </form>
 }
