13ccd87a5789b7da09e7e26ee0d4d9b4c4269d01
Danny Ayoub <danny.a95@gmail.com>
2023-12-06 00:54:37 -0600

    add source link to footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -3,6 +3,7 @@ import { Box } from "./ui/components";
 export default function Footer() {
     return <Box style={styles.container}>
         <span>created by <a href="https://ayoubd.com" target="_blank">Danny Ayoub</a></span>
+        <span><a href="https://github.com/buoyad/Tally" target="_blank">source code</a></span>
     </Box>
 }
 
@@ -13,5 +14,6 @@ const styles: { [key: string]: React.CSSProperties } = {
         padding: '12px 0',
         boxShadow: '0 -1px 2px -2px black',
         fontSize: '12px',
+        gap: '2px',
     }
 }
