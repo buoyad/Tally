76f353f10014aa5de3eb2bcd920c70b35fefca2b
Danny Ayoub <danny.a95@gmail.com>
2023-12-09 17:46:08 -0500

    add loading state for user page

---
+++ src/app/user/loading.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading, Subheading } from "../ui/components"
+
+export default function Loading() {
+    return <main>
+        <Box>
+            <Heading>My account</Heading>
+            <Subheading>Loading...</Subheading>
+        </Box>
+    </main>
+}
