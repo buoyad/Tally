561f70d6ed1f68e66ac1c65f9c1b65c17459dd78
Danny Ayoub <danny.a95@gmail.com>
2023-12-16 15:01:13 -0500

    change flow so logging in sends a verification code instead of a magic link

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -4,8 +4,9 @@ import styles from "@/app/ui/form.module.css"
 import * as validation from '@/app/lib/validation'
 import { Button } from '@/app/ui/client-components'
 import { signIn } from 'next-auth/react'
+import { Subtitle } from '../ui/components'
 
-export default function Form({ initialEmail }: { initialEmail: string | null }) {
+const EnterEmail = ({ initialEmail, onSuccess }: { initialEmail: string | null, onSuccess: (email: string) => void }) => {
     const [email, setEmail] = React.useState(initialEmail || '')
     const [disabled, setDisabled] = React.useState(true)
 
@@ -20,14 +21,63 @@ export default function Form({ initialEmail }: { initialEmail: string | null }) 
     const [pending, setPending] = React.useState(false)
     const buttonDisabled = disabled || pending
     const handleSubmit: MouseEventHandler = async (e) => {
+        console.log(email)
         setPending(true)
-        await signIn('email', { email })
+        const res = await signIn('email', { email, redirect: false })
         setPending(false)
+        if (res?.error) {
+            if (res?.url) {
+                window.location.replace(res.url);
+            }
+        } else {
+            onSuccess(email);
+        }
     }
-    return <form>
+    return <form onSubmit={(e) => e.preventDefault()}>
         <div className={styles.formSection}>
             <input id="email" name="email" type="email" placeholder="Email" className={styles.textInput} required={true} value={email} onChange={e => setEmail(e.target.value)} />
-            <Button disabled={buttonDisabled} pending={pending} label="Send me a link" pendingLabel="Sending..." role="success" onClick={handleSubmit} />
+            <Button disabled={buttonDisabled} pending={pending} label="Send me a code" pendingLabel="Sending..." role="success" onClick={handleSubmit} />
         </div>
+        <Subtitle>Tally will email you a code to create your account or login to your existing account.</Subtitle>
     </form>
+}
+
+const VerifyCode = ({ email }: { email: string }) => {
+    const [code, setCode] = React.useState('')
+    const [disabled, setDisabled] = React.useState(true)
+
+    React.useEffect(() => {
+        setDisabled(!code)
+    }, [code])
+
+    const handleSubmit = () => {
+        window.location.href =
+            `/api/auth/callback/email?email=${encodeURIComponent(email)}&token=${encodeURIComponent(code)}&callbackUrl=${encodeURIComponent('/?message=loginSuccess')}`
+    }
+
+    return <form onSubmit={(e) => e.preventDefault()}>
+        <div className={styles.formSection}>
+            <input
+                id="code"
+                name="code"
+                type="text"
+                placeholder="Verification code"
+                autoComplete='one-time-code'
+                className={styles.textInput}
+                required={true}
+                value={code} onChange={e => setCode(e.target.value)} />
+            <Button disabled={disabled} label="Verify" role="success" onClick={handleSubmit} />
+        </div>
+        <Subtitle>Enter the code you received in your email.</Subtitle>
+    </form>
+}
+
+export default function Form({ initialEmail }: { initialEmail: string | null }) {
+    const [verifyingEmail, setVerifyingEmail] = React.useState('')
+
+    if (!!verifyingEmail) {
+        return <VerifyCode email={verifyingEmail} />
+    }
+
+    return <EnterEmail initialEmail={initialEmail} onSuccess={(email) => setVerifyingEmail(email)} />
 }
