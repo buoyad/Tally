47c00b9c6422b8699afc9a200c073f44f17862be
Danny Ayoub <danny.a95@gmail.com>
2023-12-24 09:48:51 -0500

    framer motion and card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ import { GlobalTopPerformers, GlobalTopPerformersLoading, GlobalTopStreaks, Glob
 import { styleSheet } from './ui/util'
 import * as C from '@/app/lib/constants'
 import { currentPuzzleDate, displayScoreDate } from './lib/util'
+import Card from './ui/common/card'
 
 export default function Home() {
   return (
     <main>
-      <Box gap="large">
+      <Box gap="large" style={styles.overflowVisible}>
         <p>Tally is a score keeper for <a href="https://www.nytimes.com/crosswords" target="_blank">The New York Times mini crossword puzzle</a>.</p>
         <p><Link href="/tournaments">Browse tournaments</Link> or <Link href="/tournaments/create">create</Link> one of your own.</p>
         <Subheading><Link href="/score">Register today&apos;s score</Link></Subheading>
@@ -23,20 +24,24 @@ export default function Home() {
         </Suspense>
 
         <Box style={styles.gridContainer}>
-          <Box>
-            <Subheading>Fastest average</Subheading>
-            <Subtitle>Users must have at least {C.mini.minScoresForGlobalRank} scores to appear in this list</Subtitle>
-            <Suspense fallback={<GlobalTopPerformersLoading />}>
-              <GlobalTopPerformers />
-            </Suspense>
-          </Box>
-          <Box>
-            <Subheading>Top streaks</Subheading>
-            <Subtitle><br /></Subtitle>
-            <Suspense fallback={<GlobalTopStreaksLoading />}>
-              <GlobalTopStreaks />
-            </Suspense>
-          </Box>
+          <Card>
+            <Box>
+              <Subheading>Fastest average</Subheading>
+              <Subtitle>Users must have at least {C.mini.minScoresForGlobalRank} scores to appear in this list</Subtitle>
+              <Suspense fallback={<GlobalTopPerformersLoading />}>
+                <GlobalTopPerformers />
+              </Suspense>
+            </Box>
+          </Card>
+          <Card>
+            <Box>
+              <Subheading>Top streaks</Subheading>
+              <Subtitle><br /></Subtitle>
+              <Suspense fallback={<GlobalTopStreaksLoading />}>
+                <GlobalTopStreaks />
+              </Suspense>
+            </Box>
+          </Card>
         </Box>
       </Box>
     </main>
@@ -49,8 +54,10 @@ const styles = styleSheet({
     gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
     gap: '1rem',
     width: '100%',
+    overflow: 'visible',
   },
   fullWidth: {
     gridColumn: '1 / -1'
   },
+  overflowVisible: { overflow: 'visible' }
 })
---
+++ src/app/ui/common/card.tsx
@@ -0,0 +1,29 @@
+'use client'
+import * as React from 'react'
+import { styleSheet } from '../util'
+import { motion } from 'framer-motion'
+
+type Props = {
+    children: React.ReactNode
+    style?: React.CSSProperties
+}
+export default function Card({ children, style }: Props) {
+    const rotate = Math.random() * 4 - 2
+    return <motion.div style={{ ...styles.container, ...style }}
+        initial={{ scale: .95, filter: 'blur(5px)' }}
+        whileInView={{ filter: 'blur(0)', scale: 1, rotate }}
+        transition={{ type: 'spring', bounce: 0, damping: 20, filter: { duration: .5 } }}
+    >
+        {children}
+    </ motion.div>
+}
+
+
+const styles = styleSheet({
+    container: {
+        border: '1px solid var(--color-boxShadow)',
+        boxShadow: '2px 2px var(--color-boxShadow)',
+        padding: '24px',
+        backgroundColor: 'var(--color-navBackground)'
+    }
+})
