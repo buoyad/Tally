e926c9864e3c5ac8ef978bce55a247fbb47db70c
Danny Ayoub <danny.a95@gmail.com>
2023-12-27 10:18:40 -0500

    fix displaySeconds adding an extra digit when toFixed rounds a 9.XX to a 10

--- src/app/lib/util.ts
+++ src/app/lib/util.ts
@@ -9,7 +9,7 @@ export const displaySeconds = (seconds: number) => {
     const minutes = Math.floor(seconds / 60)
     const remainder = seconds % 60
     const remainderStr = remainder === Math.floor(remainder) ? remainder.toString() : remainder.toFixed(1)
-    return `${minutes}:${remainder < 10 ? '0' : ''}${remainderStr}`
+    return `${minutes}:${remainderStr.length === 1 ? '0' : ''}${remainderStr}`
 }
 
 export const displayScoreDate = (date: string) => {
